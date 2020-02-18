481f58b24c944c7e14526458f5ebf6e8a7107624
刘建辉 <jianhui@yzyw-inc.com>
2020-02-18 16:51:01 +0800

    feat(main/index.js): 如果是mac,不直接退出,做了判断

--- app/main/index.js
+++ app/main/index.js
@@ -1,6 +1,6 @@
 const { getOrCreateMainWindow } = require('./windows');
 const { setupDevTools } = require('./devtools');
-const { app } = require('electron');
+const { app, BrowserWindow } = require('electron');
 require('./event');
 
 const onReady = async () => {
@@ -10,6 +10,20 @@ const onReady = async () => {
 
 function main(){
   app.on('ready', onReady);
+  app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+      app.quit();
+    }
+    app.on('activate', () => {
+      // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+      // 通常在应用程序中重新创建一个窗口。
+      if (BrowserWindow.getAllWindows().length === 0) {
+        getOrCreateMainWindow();
+      }
+    });
+  });
 }
 
 main();
